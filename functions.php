844cb54cbb7f6f9d80710ee14a4e048d658ab206
Asiqur Rahman <asikur22@gmail.com>
2025-07-09 14:56:12 +0600

    initial

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+
+function asiq_ele_minimal_theme_setup() {
+	add_theme_support( 'title-tag' );
+	add_theme_support( 'post-thumbnails' );
+}
+
+add_action( 'after_setup_theme', 'asiq_ele_minimal_theme_setup' );
+
+function asiq_ele_minimal_theme_scripts() {
+	wp_enqueue_style( 'asiq-ele-minimal-style', get_stylesheet_uri() );
+}
+
+add_action( 'wp_enqueue_scripts', 'asiq_ele_minimal_theme_scripts' );
